37e54c98b1f6d40e0e6e80fabb3b09a41e4d831b
Ivan Zolotarev <zolotarev.ivan.n@yandex.ru>
2015-07-25 19:23:18 +0600

    Добавляет собственный обработчик ошибок ErrorHandler

--- include/config.php
+++ include/config.php
@@ -11,3 +11,24 @@ define('SMARTY_DIR', SITE_ROOT . '/libs/smarty/');
 define('TEMPLATE_DIR', PRESENTATION_DIR . 'templates');
 define('COMPILE_DIR', PRESENTATION_DIR . 'templates_c');
 define('CONFIG_DIR', SITE_ROOT . '/include/configs');
+
+// Эти значения должны быть равны true на этапе разработки
+define('IS_WARNING_FATAL', true);
+define('DEBUGGING', true);
+
+// Типы ошибок, о которых должны составляться сообщения
+define('ERROR_TYPES', E_ALL);
+
+// Настройки отправки сообщений администраторам по электронной почте
+define('SEND_ERROR_MAIL', false);
+define('ADMIN_ERROR_MAIL', 'Administrator@example.com');
+define('SENDMAIL_FROM', 'Errors@example.com');
+ini_set('sendmail_from', SENDMAIL_FROM);
+
+// По умолчанию мы не записываем сообщения в журнал
+define('LOG_ERRORS', false);
+define('LOG_ERRORS_FILE', 'F:\\иван\\OpenServer\\domains\\simpleshop');
+
+/* Общее сообщение об ошибке, которое должно отображаться вместо 
+подробной информации (если DEBUGGING равно false)*/
+define('SITE_GENERIC_ERROR_MESSAGE', '<h1>TShirtShop Error!</h1>');
--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php 
 // Подключем служебные файлы
 require_once 'include/config.php';
+require_once BUSINESS_DIR . 'error_handler.php';
+
+// Задаем обработчик ошибок
+ErrorHandler::SetHandler();
 
 // Загружаем шаблон страницы приложения
 require_once PRESENTATION_DIR . 'application.php';
